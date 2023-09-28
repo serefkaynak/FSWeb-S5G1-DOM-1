7cb3828bf25a25275340260e2db5e3e0e7084b9a
Şeref Kaynak <serefkynk@gmail.com>
2023-09-28 22:14:12 +0300

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
-  "nav": {
+  nav: {
     "nav-item-1": "Servisler",
     "nav-item-2": "Ürünler",
     "nav-item-3": "Vizyon",
@@ -7,7 +7,7 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
     "nav-item-5": "Hakkımızda",
     "nav-item-6": "İletişim",
   },
-  "cta": {
+  cta: {
     "h1": "Bu DOM Mükemmel",
     "button": "Başlayın",
   },
@@ -23,16 +23,16 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
     "vizyon-h4": "Vizyon",
     "vizyon-içeriği": "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "iletisim": {
+  iletisim: {
     "iletişim-h4": "İletişim",
     "adres": "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
     "telefon": "+90 (123) 456-7899",
     "email": "satis@birsirketsitesi.com.tr",
   },
-  "footer": {
+  footer: {
     "copyright": "Copyright Bir Şirket Sitesi 2022",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
@@ -49,26 +49,57 @@ const navLinks = nav.querySelectorAll('a');
 const navContents = Object.values(siteContent.nav);
 
 navLinks.forEach((link, index) => {
+  link.className = "italik";
   link.textContent = navContents[index];
 });
 /* NAV HEADER END */
 
 /* LOGO START */
 const logoImg = document.querySelector('#logo-img');
-logoImg.src = siteContent.images['logo-img'];
+logoImg.setAttribute('src', siteContent.images['logo-img']);
 /* LOGO END */
 
 /* CTA START */
 const ctaText = document.querySelectorAll('.cta-text h1');
-ctaText.textContent = siteContent.cta['h1'];
+ctaText.textContent = siteContent.cta.h1;
 
-const ctaButton = document.querySelector('.cta-button button');
-ctaButton.textContent = siteContent.cta['button'];
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent.cta.button;
 
-const ctaImg = document.querySelector('.cta-img img');
-ctaImg.src = siteContent.images['cta-img'];
+const ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent.images['cta-img']);
 /* CTA END */
 
 /* CONTENT START */
-const textContents = document.querySelector('.text-content h4');
-textContents.textContent = siteContent.anaIçerik['özellikler-h4'];
+const h4list = document.querySelectorAll('.text-content h4');
+const plist = document.querySelectorAll('.text-content p');
+
+h4list[0].textContent = siteContent['ana-içerik']['hakkımızda-h4'];
+plist[0].textContent = siteContent['ana-içerik']['hakkımızda-içerik'];
+
+h4list[1].textContent = siteContent['ana-içerik']['servisler-h4'];
+plist[1].textContent = siteContent['ana-içerik']['özellikler-içeriği'];
+
+h4list[2].textContent = siteContent['ana-içerik']['ürünler-h4'];
+plist[2].textContent = siteContent['ana-içerik']['ürünler-içerik'];
+
+h4list[3].textContent = siteContent['ana-içerik']['vizyon-h4'];
+plist[3].textContent = siteContent['ana-içerik']['vizyon-içeriği'];
+
+h4list[4].textContent = siteContent['ana-içerik']['özellikler-h4'];
+plist[4].textContent = siteContent['ana-içerik']['özellikler-içerik'];
+
+const img3 = document.getElementById('middle-img');
+img3.setAttribute('src', siteContent.images['accent-img']);
+
+const h4Element = document.querySelector(".contact h4");
+h4Element.textContent = siteContent.iletisim['iletişim-h4'];
+
+const contactPList = document.querySelectorAll(".contact p");
+contactPList[0].textContent = siteContent.iletisim.adres;
+contactPList[1].textContent = siteContent.iletisim.telefon;
+contactPList[2].textContent = siteContent.iletisim.email;
+
+const footerText = document.querySelector("footer a");
+footerText.textContent = siteContent.footer.copyright;
+footerText.className = "bold";
